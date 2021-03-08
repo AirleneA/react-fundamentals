489aa5b546a6d22d223a78b24c783a0e4c9bd045
AirleneA <airlene.antonelli@gmail.com>
2021-03-08 13:56:01 +0000

    (08/03) Exercícios 04 e 05

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -3,7 +3,6 @@
 
 import * as React from 'react'
 import '../box-styles.css'
-
 // 💰 Use the className for the size and style (backgroundColor) for the color
 // 💰 each of the elements should also have the "box" className applied
 
@@ -12,18 +11,26 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+//trocou smalBox por box 1 ...os pontinhos é porque a variável não está sendo usada
+const smallBox = <div>className="box box--small" style= {{backgroudColor: 'lightblue', fontStyle:'italic'}}>small ligthbule box</div>
+const mediumBox = <div>className="box box--medium" style= {{backgroudColor: 'pink', fontStyle:'italic'}}>small ligthbule box</div>
+const largeBox = <div>className="box box--large" style= {{backgroudColor: 'lorange', fontStyle:'italic'}}>small ligthbule box</div>
 
-function App() {
-  return (
-    <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
-    </div>
-  )
-}
+//Por padrão, toda função de componente recebe todas as props
+//como um único objeto. Caso queiramos lidar com uma prop em
+//particular, precisamos usar desestruturação para isolá-la.
+    function Box(className, style, ...props){
+       const className = 'box--' + size
+        return<div classname={"box " + classname} style={{fontStyle: 'italic', ...style}} {...props}/>
+    }
 
-export default App
+    function App() {
+        return (
+            <div>
+            <Box id="box1" lang="en" size="small" style={{backgoundColor: 'lightblue'}}>Small LightBlue Box</Box>
+            <Box id="box2" lang="en"size="medium" style={{backgoundColor: 'pink'}}>Medium Pink Box</Box>
+            <Box id="box3" lang="en"size="large" style={{backgoundColor: 'orange'}}>large Orange Box</Box>
+        </div>
+        )
+    }
+
