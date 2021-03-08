ec7994b342f55d76089da505a4d13e6e049fd546
AirleneA <airlene.antonelli@gmail.com>
2021-03-08 14:08:33 +0000

    (08/03) Exercícios 04 e 05correção

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -3,6 +3,7 @@
 
 import * as React from 'react'
 import '../box-styles.css'
+
 // 💰 Use the className for the size and style (backgroundColor) for the color
 // 💰 each of the elements should also have the "box" className applied
 
@@ -11,26 +12,26 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-//trocou smalBox por box 1 ...os pontinhos é porque a variável não está sendo usada
-const smallBox = <div>className="box box--small" style= {{backgroudColor: 'lightblue', fontStyle:'italic'}}>small ligthbule box</div>
-const mediumBox = <div>className="box box--medium" style= {{backgroudColor: 'pink', fontStyle:'italic'}}>small ligthbule box</div>
-const largeBox = <div>className="box box--large" style= {{backgroudColor: 'lorange', fontStyle:'italic'}}>small ligthbule box</div>
+const smallBox = <div className="box box--small" style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}>small lightblue box</div>
+const mediumBox = <div className="box box--medium" style={{backgroundColor: 'pink', fontStyle: 'italic'}}>medium pink box</div>
+const largeBox = <div className="box box--large" style={{backgroundColor: 'orange', fontStyle: 'italic'}}>large orange box</div>
 
-//Por padrão, toda função de componente recebe todas as props
-//como um único objeto. Caso queiramos lidar com uma prop em
-//particular, precisamos usar desestruturação para isolá-la.
-    function Box(className, style, ...props){
-       const className = 'box--' + size
-        return<div classname={"box " + classname} style={{fontStyle: 'italic', ...style}} {...props}/>
-    }
+// Por padrão, toda função de componente recebe todas as props
+// como um único objeto. Caso queiramos lidar com uma prop em
+// particular, precisamos usar desestruturação para isolá-la.
+function Box({style, size = 'small', ...props}) {
+  const className = 'box--' + size
+  return <div className={"box " + className} style={{fontStyle: 'italic', ...style}} {...props} />
+}
 
-    function App() {
-        return (
-            <div>
-            <Box id="box1" lang="en" size="small" style={{backgoundColor: 'lightblue'}}>Small LightBlue Box</Box>
-            <Box id="box2" lang="en"size="medium" style={{backgoundColor: 'pink'}}>Medium Pink Box</Box>
-            <Box id="box3" lang="en"size="large" style={{backgoundColor: 'orange'}}>large Orange Box</Box>
-        </div>
-        )
-    }
+function App() {
+  return (
+    <div>
+      <Box id="box1" lang="en" size="small" style={{backgroundColor: 'lightblue'}}>Small LightBlue Box</Box>
+      <Box id="box2" lang="en" size="medium" style={{backgroundColor: 'pink'}}>Medium Pink Box</Box>
+      <Box id="box3" lang="en" size="large" style={{backgroundColor: 'orange'}}>Large Orange Box</Box>
+    </div>
+  )
+}
 
+export default App
